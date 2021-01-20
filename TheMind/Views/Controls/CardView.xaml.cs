45dff4d519c2969ef8e6cc5360cb5463c877cc52
Francisco <francisco.dechoudens@abarcahealth.com>
2021-01-19 21:51:37 -0400

    Add Card Value

--- TheMind/Views/Controls/CardView.xaml.cs
+++ TheMind/Views/Controls/CardView.xaml.cs
@@ -6,6 +6,18 @@ namespace TheMind.Views.Controls
 {
     public partial class CardView : ContentView
     {
+        public static readonly BindableProperty CardValueProperty = BindableProperty.Create(
+        nameof(CardValue),        
+        typeof(int),     
+        typeof(CardView),   
+        0);      
+
+        public int CardValue
+        {
+            get => (int)GetValue(CardView.CardValueProperty);
+            set => SetValue(CardView.CardValueProperty, value);
+        }
+
         public CardView()
         {
             InitializeComponent();
